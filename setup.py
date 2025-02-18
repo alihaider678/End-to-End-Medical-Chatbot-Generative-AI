d13607a92b5d9ebc62ae96b61312dfef9c6517f1
Ali Haider <ali.haiyder11@gmail.com>
2025-02-19 01:45:12 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ali Haider',
+    author_email= 'ali.haiyder11@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
